b4de538a9a4efb4285b4ef65a17a5ae41137d812
omkargodse <anurag.godse@gmail.com>
2022-09-02 14:58:49 +0530

    End of section 2

---
+++ NZWalks/NZWalks.Api/Models/Domains/Region.cs
@@ -0,0 +1,20 @@
+﻿namespace NZWalks.Api.Models.Domains
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+        public double Lat { get; set; }
+
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        //Navigation property ( 1 Region has Many walks)
+        public IEnumerable<Walk> Walks  { get; set; }
+    }
+}
---
+++ NZWalks/NZWalks.Api/Models/Domains/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.Api.Models.Domains
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        // Navigation property
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
